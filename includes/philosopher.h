67f988b2baa282c0015252766c2d038f71046b7c
Javier Cervilla miguel <jcervill@c3r6s2.42madrid.com>
2022-03-16 13:37:55 +0100

    feat(NORM):[+] includes OK

--- includes/philosopher.h
+++ includes/philosopher.h
@@ -6,29 +6,25 @@
 /*   By: jcervill <jcervill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 23:53:58 by jcervill          #+#    #+#             */
-/*   Updated: 2022/03/16 11:36:57 by jcervill         ###   ########.fr       */
+/*   Updated: 2022/03/16 13:34:51 by jcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSHOPHER_H
-#define PHILOSHOPHER_H
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <string.h>
-#include <sys/time.h>
-
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <string.h>
+# include <sys/time.h>
 # define MAX_INT 2147483647
-
 # define TAKING_FORK "has taken a fork\n"
 # define EATING "is eating\n"
 # define THINKING "is thinking\n"
 # define SLEEPING "is sleeping\n"
 # define DROPING_FORK "has dropen a fork\n"
 # define DYING "died"
-
-
 # define ERR_BAD_ARGS "\
 Usage: ./philosopher <number of philosophers> \
 <time to die> <time to eat> <time to sleep> \
@@ -36,10 +32,8 @@ Usage: ./philosopher <number of philosophers> \
 # define ERR_INIT "Error: Initiaizing program."
 # define ERR_INIT_PHILO "Error: Initializing Philos."
 # define ERR_INIT_TH "Error: Initializing Philo`s threads."
-
 # define STATUS_NO_ERR 0
 # define STATUS_ERR 1
-
 
 typedef enum e_args
 {
@@ -49,20 +43,20 @@ typedef enum e_args
 	TIME_TO_SLEEP,
 	NUM_TIMES_EAT,
 	NUM_ARGS
-} t_args;
+}	t_args;
 
 typedef enum e_boolean
 {
 	FALSE,
 	TRUE
-} t_boolean;
+}	t_boolean;
 
 typedef enum e_forks
 {
 	LEFT,
 	RIGHT,
 	BOTH
-} t_forks;
+}	t_forks;
 
 typedef enum e_status
 {
@@ -70,14 +64,14 @@ typedef enum e_status
 	EAT,
 	SLEEP,
 	DEAD
-} t_status;
+}	t_status;
 
 typedef struct s_mutex
 {
-	pthread_mutex_t mutex;
-} t_mutex;
+	pthread_mutex_t	mutex;
+}	t_mutex;
 
-typedef struct	s_philo
+typedef struct s_philo
 {
 	int				id;
 	t_status		status;
@@ -88,7 +82,7 @@ typedef struct	s_philo
 	long long		last_eat;
 	int				times_eat;
 	struct s_data	*dt;
-} t_philo;
+}	t_philo;
 
 typedef struct s_data
 {
@@ -100,13 +94,13 @@ typedef struct s_data
 	pthread_mutex_t	start;
 	pthread_mutex_t	typing;
 	pthread_mutex_t	*forks;
-} t_data;
+}	t_data;
 
 // UTILS
 void		ft_bzero(void *str, size_t n);
 int			ft_atoi(char *str);
-long long	ft_get_current_time();
-long long	diff_time (long long t1, long long t2);
+long long	ft_get_current_time(void);
+long long	diff_time(long long t1, long long t2);
 t_boolean	ft_isdigit(int c);
 void		smart_sleep(int time);
 int			ft_strcmp(const char *s1, const char *s2);
@@ -125,8 +119,8 @@ void		ft_clean(t_data *data);
 void		*eat_think_sleep(void *philo_address);
 //MAIN
 void		eating_routine(t_philo *philo);
-t_boolean	ft_check_right_fork (t_philo *philo);
-t_boolean	ft_check_left_fork (t_philo *philo);
+t_boolean	ft_check_right_fork(t_philo *philo);
+t_boolean	ft_check_left_fork(t_philo *philo);
 void		take_right_fork(t_philo *philo);
 void		take_left_fork(t_philo *philo);
 void		ft_eat(t_philo *philo);
